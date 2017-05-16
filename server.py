63ddd5d786fee448b9a66a88f57aabd111edff20
Hannah Schafer <schafer.hannah@gmail.com>
2017-05-15 21:13:51 -0700

    pip installed flask and flask_debugtoolbar, html template up and running with form and submit button

--- server.py
+++ server.py
@@ -2,10 +2,12 @@
 """The Markov tweet generator flask app server file."""
 
 from flask import (Flask, jsonify, render_template, request, make_response)
-
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "752398u5&9563!!hms"
 
 @app.route('/')
 def splashpage():
@@ -35,5 +37,10 @@ def send_tweet():
 
 if __name__ == "__main__":
 
+    app.debug = True
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
 
     app.run(port=5000, host='0.0.0.0')
+
